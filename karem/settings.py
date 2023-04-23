336c170605d96243e55b5c1dc3073bf38b40623c
ABDNOAMAN <114725229+ABDNOAMAN@users.noreply.github.com>
2023-04-23 14:40:11 +0200

    Update settings.py

--- karem/settings.py
+++ karem/settings.py
@@ -7,6 +7,19 @@ NEWSPIDER_MODULE = "karem.spiders"
 
 
 
+# Add Your ScrapeOps API Key
+SCRAPEOPS_API_KEY = 'e4ea08af-ef35-4354-885d-e75b34979a52'
+
+# Add In The ScrapeOps Extension
+EXTENSIONS = {
+        'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+        }
+
+# Update The Download Middlewares
+DOWNLOADER_MIDDLEWARES = {
+'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
+'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+}
 DOWNLOAD_HANDLERS = {
     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
